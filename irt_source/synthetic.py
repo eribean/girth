8df52eceb134768624c327854b4a2139710565bc
RyanSchool <rsanchez44@gatech.edu>
2019-11-12 14:17:24 -0500

    Adding synthetic data creation.

---
+++ irt_source/synthetic.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+
+class SyntheticIRT(object):
+    """Class to create synthetic irt data.
+
+    Args:
+        Abilities: (sp.stats, args*)
+        Discrimination:
+
+    """
+
+    def __init__(self):
+        pass

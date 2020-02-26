c80d35e393719586fdfae43f71669646eeec8be3
RyanSchool <rsanchez44@gatech.edu>
2020-02-26 15:15:41 -0500

    Added all the appropriate methods to solve for unidimensional parameters.

--- irt_source/__init__.py
+++ irt_source/__init__.py
@@ -1,3 +1,7 @@
 from .utils import irt_evaluation
 from .synthetic import create_synthetic_irt_dichotomous
-from .approximation_methods import rauch_estimate, onepl_estimate, twopl_estimate
+from .mml_approximation_methods import rauch_approx, onepl_approx, twopl_approx
+from .mml_separate_methods import rauch_separate, onepl_separate, twopl_separate
+from .mml_full_methods import rauch_full, onepl_full, twopl_full
+from .conditional_methods import rasch_conditional
+from .jml_methods import rauch_jml, onepl_jml, twopl_jml
---
+++ irt_source/conditional_methods.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+from scipy.optimize import fminbound
+
+
+def rasch_conditional(dataset, discrimination=1, max_iter=25):
+    """
+        Estimates the difficulty parameters in a rasch model
+
+        Args:
+            dataset: [items x participants] matrix of True/False Values
+            discrimination: scalar of discrimination used in model (default to 1)
+            max_iter: maximum number of iterations to run
+
+        Returns:
+            array of discrimination estimates
+
+        Notes:
+            This uses conditional likelihood and requires setting an
+            identifying value,  this functions requires the mean of the
+            difficulty estimates to be zero
+    """
+    n_items = dataset.shape[0]
+    unique_sets, counts = np.unique(dataset, axis=1, return_counts=True)
+
+    # Initialize all the difficulty parameters to zeros
+    # Set an identifying_mean to zero
+    ##TODO: Add option to specifiy position
+    betas = np.zeros((n_items, ))
+    identifying_mean = 0.0
+
+    # Remove the zero and full count values
+    if(unique_sets[:, 0].sum() == 0):
+        unique_sets = np.delete(unique_sets, 0, axis=1)
+        counts = np.delete(counts, 0)
+
+    if(unique_sets[:, -1].sum() == n_items):
+        unique_sets = np.delete(unique_sets, -1, axis=1)
+        counts = np.delete(counts, -1)
+
+    response_set_sums = unique_sets.sum(axis=0)
+
+    def _denominator(betas):
+        """Computes the symmetric functions based on the betas
+
+         Indexes by score, left to right
+
+        """
+        polynomials = np.c_[np.ones_like(betas), np.exp(-betas)]
+
+        # This is an easy way to compute all the values at once,
+        # not necessarily the fastest
+        otpt = 1
+        for polynomial in polynomials:
+            otpt = np.convolve(otpt, polynomial)
+        return otpt
+
+    for iteration in range(max_iter):
+        previous_betas = betas.copy()
+
+        for ndx in range(n_items):
+            partial_conv = _denominator(np.delete(betas, ndx))
+
+            def min_func(estimate):
+                betas[ndx] = estimate
+                full_convolution = np.convolve([1, np.exp(-estimate)], partial_conv)
+
+                numerator = np.exp(-np.sum(unique_sets * betas[:,None], axis=0))
+                denominator = full_convolution[response_set_sums]
+
+                return -np.log(numerator / denominator).dot(counts)
+
+            # Solve for the difficulty parameter
+            betas[ndx] = fminbound(min_func, -5, 5)
+
+            # recenter
+            betas += (identifying_mean - betas.mean())
+
+        # Check termination criterion
+        if np.abs(betas - previous_betas).max() < 1e-3:
+            break
+
+    return betas / discrimination

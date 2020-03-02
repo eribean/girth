d84139bd5ee693ecea05da07f8d05927f433fcae
RyanSchool <rsanchez44@gatech.edu>
2020-03-02 14:32:37 -0500

    Added graded response model to synthetic creation.

--- girth/synthetic.py
+++ girth/synthetic.py
@@ -22,7 +22,7 @@ def create_correlated_abilities(correlation_matrix, n_participants):
 
 
 def create_synthetic_irt_dichotomous(difficulty, discrimination, thetas,
-                                     seed=None):
+                                     guessing=0, seed=None):
     """
         Creates synthetic IRT data to test parameters estimation
         functions.  Only for use with dichotomous outputs
@@ -34,6 +34,7 @@ def create_synthetic_irt_dichotomous(difficulty, discrimination, thetas,
             difficulty: [array] of difficulty parameters
             discrimination:  [array | number] of discrimination parameters
             thetas: [array] of person abilities
+            guessing: [array | number] of guessing parameters associated with items
             seed: Optional setting to reproduce results
 
         Returns:
@@ -43,7 +44,14 @@ def create_synthetic_irt_dichotomous(difficulty, discrimination, thetas,
     if seed:
         np.random.seed(seed)
 
+    if np.ndim(guessing) < 1:
+        guessing = np.full_like(difficulty, guessing)
+
     continuous_output = irt_evaluation(difficulty, discrimination, thetas)
+
+    # Add guessing parameters
+    continuous_output *= (1.0 - guessing[:, None])
+    continuous_output += guessing[:, None]
 
     # convert to binary based on probability
     random_compare = np.random.rand(*continuous_output.shape)
@@ -97,3 +105,100 @@ def create_synthetic_mirt_dichotomous(difficulty, discrimination, thetas,
     random_compare = np.random.rand(*continuous_output.shape)
 
     return random_compare <= continuous_output
+
+
+## Private functions for polytomous outputs
+def _my_digitize(the_input):
+    """Private function to compute polytomous levels.
+       The input has been concatenated to use the
+       vectorize functions (value, thresholds)
+    """
+    return np.searchsorted(the_input[1:], the_input[0])
+
+    
+def _graded_func(difficulty, discrimination, thetas, output):
+    """Private function to compute the probabilities for
+       the graded response model.  This is done in place
+       and does not return anything
+   """
+    # Do first level
+    output[0] = 1.0 - irt_evaluation(np.array([difficulty[0]]), 
+                                     discrimination, thetas)
+    
+    for level_ndx in range(1, output.shape[0]-1):
+        right = irt_evaluation(np.array([difficulty[level_ndx]]), 
+                               discrimination, thetas)
+        left = irt_evaluation(np.array([difficulty[level_ndx-1]]),
+                              discrimination, thetas)
+        output[level_ndx] = left - right
+        
+    # Do last level
+    output[-1] = irt_evaluation(np.array([difficulty[-1]]),
+                                discrimination, thetas)
+
+
+def _credit_func(difficulty, discrimination, thetas, output):
+    """Private function to compute the probabilities for
+       the partial credit model.  This is done in place
+       and does not return anything
+    """
+    pass
+
+
+def create_synthetic_irt_polytomous(difficulty, discrimination, thetas,
+                                    model='grm', seed=None):
+    """
+        Creates synthetic IRT data to test parameters estimation
+        functions.  Creates polytomous output with specified number
+        of levels from [1, levels]
+
+        Args:
+            difficulty: [2D array (items x n_levels-1)] of difficulty parameters
+            discrimination:  [array | number] of discrimination parameters
+            thetas: [array] of person abilities
+            model: ["grm", "pcm] string specifying which polytomous model to use
+                    'grm': Graded Response Model
+                    'pcm': Generalized Partial Credit Model
+            seed: Optional setting to reproduce results
+
+        Returns:
+            dichotomous matrix of [difficulty.size x thetas.size] representing
+            synthetic data
+    """
+    n_items, n_levels = difficulty.shape
+
+    if seed:
+        np.random.seed(seed)
+    
+    # Check for single input of discrimination
+    if (np.ndim(discrimination) < 1) or (np.shape(discrimination)[0] == 1):
+        discrimination = np.full((n_items,), discrimination)
+
+    # Get the model to use, will throw error if not supported
+    probability_func = {'grm': _graded_func, 'pcm': _credit_func}[model.lower()]
+        
+    if n_levels == 1:
+        raise AssertionError("Polytomous items must have more than 1 threshold")
+
+    # Initialize output for memory concerns        
+    level_scratch = np.zeros((n_levels + 2, thetas.size))
+    output = np.zeros((n_items, thetas.size), dtype='int')
+    
+    # Loop over items and compute probability estimates
+    # for each of the levels and assign level based on 
+    # those probabilities
+    for item_ndx in range(n_items):
+        # Obtain the probabilities for the data (in-place)
+        probability_func(difficulty[item_ndx], discrimination[item_ndx], 
+                         thetas, level_scratch[1:, :])
+
+        # Get the thresholds of the levels
+        np.cumsum(level_scratch[1:, :], axis=0, out=level_scratch[1:, :])
+        level_scratch[0] = np.random.rand(thetas.size)
+        
+        # Discritize the outputs based on the thresholds
+        output[item_ndx] = np.apply_along_axis(_my_digitize, axis=0, arr=level_scratch)
+    
+    # Add 1 to return [1, n_levels]
+    output += 1
+    return output    

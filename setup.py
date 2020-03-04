7aca6c864f34badbff44d05098a65452e2e0717a
RyanSchool <rsanchez44@gatech.edu>
2020-03-04 12:44:14 -0500

    Added long_description to setup.py to add markdown to pypi project package.

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 if __name__ == '__main__':
     setup(
         name="girth", 
         packages=['girth'],
-        version="0.1",
+        version="0.1.1",
         license="MIT",
         description="A python package for Item Response Theory.",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         author='Ryan C. Sanchez',
         author_email='rsanchez44@gatech.edu',
         url = 'https://github.com/eribean/girth',

4e31bd0fc4447b013711bf275aae43d4dce9bc48
Ryan S <eribean1@gmail.com>
2020-03-23 16:51:38 -0400

    Added Expected a posterior methods.

--- girth/ability_methods.py
+++ girth/ability_methods.py
@@ -1,9 +1,11 @@
 import numpy as np
+
+from scipy import integrate
 from scipy.stats import uniform
 from scipy.stats import norm as gaussian
 from scipy.optimize import fminbound
-
 from girth import convert_responses_to_kernel_sign
+from girth.utils import _compute_partial_integral, _get_quadrature_points
 
 
 def ability_map(dataset, difficulty, discrimination, distribution=None):
@@ -88,3 +90,50 @@ def ability_mle(dataset, difficulty, discrimination, no_estimate=np.nan):
     thetas[mask2] *= -1    
 
     return thetas
+
+
+def ability_eap(dataset, difficulty, discrimination, distribution=None):
+    """Estimates the abilities for dichotomous models.
+
+    Estimates the ability parameters (theta) for dichotomous models via
+    expaected a posterior likelihood estimation.
+
+    Args:
+        dataset: [n_items, n_participants] (2d Array) of measured responses
+        difficulty: (1d Array) of difficulty parameters for each item
+        discrimination: (1d Array) of disrimination parameters for each item
+        distribution: function handle to PDF of ability distribution, p = f(theta)
+                      the default is gaussian (i.e: scipy.stats.norm(0, 1).pdf)
+
+    Returns
+        1d array of estimated abilities
+    """
+    if distribution is None:
+        distribution = gaussian(0, 1).pdf
+    
+    discrimination = np.atleast_1d(discrimination)
+    if discrimination.size == 1:
+        discrimination = np.full(dataset.shape[0], discrimination)
+
+    the_sign = convert_responses_to_kernel_sign(dataset)
+
+    theta = _get_quadrature_points(61, -5, 5)
+    partial_int = _compute_partial_integral(theta, difficulty, discrimination, the_sign)
+
+    # Need to remove guassian distribution that is appended in the partial integration
+    remove_disribution = np.sqrt(2 * np.pi) / np.exp(-np.square(theta) / 2)
+    new_distribution = distribution(theta) * remove_disribution
+    partial_int *= new_distribution
+
+    # Compute the denominator
+    denominator = integrate.fixed_quad(lambda x: partial_int, -5, 5, n=61)
+
+    # compute the numerator
+    partial_int *= theta
+
+    numerator = integrate.fixed_quad(lambda x: partial_int, -5, 5, n=61)
+
+    return numerator / denominator
+
+
+    
